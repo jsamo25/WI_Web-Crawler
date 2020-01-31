04a395d99589b1372321dfa88cb3779f898c8195
jsamo25 <56796144+jsamo25@users.noreply.github.com>
2020-01-31 17:03:01 +0100

    Init.

--- HTML Parser.py
+++ HTML Parser.py
@@ -1,2 +1,5 @@
 # HTML Parser: https://docs.python.org/3/library/html.parser.html
 # Web Scrapper: http://scrapingauthority.com/2016/08/25/web-scraping-beautifulsoup/
+# Check on www.website/robots.txt
+
+import beautifulsoup4 as bs4

095ca222718f88b8d589e54cb409bf521b7ca67e
Jorge <jsamo25@gmail.com>
2020-02-26 19:14:27 +0100

    Using Index.html to test BeautifulSoup Library

--- HTML Parser.py
+++ HTML Parser.py
@@ -2,4 +2,15 @@
 # Web Scrapper: http://scrapingauthority.com/2016/08/25/web-scraping-beautifulsoup/
 # Check on www.website/robots.txt
 
-import beautifulsoup4 as bs4
+
+from bs4 import BeautifulSoup
+from pdb import set_trace
+
+with open ("index.html", "r") as f:
+    contents = f.read()
+    soup = BeautifulSoup(contents, "lxml")
+
+    for tag in soup.find_all("li"):
+        print("{0}: {1}".format(tag.name, tag.text))
+
+#following tutorial from: http://zetcode.com/python/beautifulsoup/

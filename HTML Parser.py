d5e143e34dbaf371a4e71fca92b3b8ececb21e9d
Jorge <jsamo25@gmail.com>
2020-03-06 19:25:10 +0100

    Web Crawler Working: missing: check for robots.txt

--- HTML Parser.py
+++ HTML Parser.py
@@ -2,15 +2,58 @@
 # Web Scrapper: http://scrapingauthority.com/2016/08/25/web-scraping-beautifulsoup/
 # Check on www.website/robots.txt
 
-
+from collections import deque
 from bs4 import BeautifulSoup
-from pdb import set_trace
+from urlparse import urljoin
+import sys
+import urllib2
 
-with open ("index.html", "r") as f:
-    contents = f.read()
-    soup = BeautifulSoup(contents, "lxml")
 
-    for tag in soup.find_all("li"):
-        print("{0}: {1}".format(tag.name, tag.text))
+#url = raw_input("Input the url to start the crawling (example: http://www.google.com ):")
+url = "http://www.google.com"
+queue, visited_links= deque([]), []
 
-#following tutorial from: http://zetcode.com/python/beautifulsoup/
+
+def bfs_crawler(url):
+    count_limit = 10
+    visited_links.append(url)
+    if len(queue) > count_limit:
+        return
+
+    url_file = urllib2.urlopen(url)
+    soup = BeautifulSoup(url_file.read())
+    url_list = soup.findAll("a", href=True)
+
+    for link in url_list:
+        flag = 0
+        # Complete relative URLs and strip trailing slash
+        complete_url = urljoin(url, link["href"]).rstrip('/')
+
+        # Check if the URL already exists in the queue
+        for j in queue:
+            if j == complete_url:
+                flag = 1
+                break
+
+        # If not found in queue
+        if flag == 0:
+            if len(queue) > count_limit:
+                return
+            if (visited_links.count(complete_url)) == 0:
+                queue.append(complete_url)
+
+    # Pop one URL from the queue from the left side so that it can be crawled
+    current = queue.popleft()
+    # Recursive call to crawl until the queue is populated with 100 URLs
+    bfs_crawler(current)
+
+
+
+bfs_crawler(url)
+
+# Print queue
+for i in queue:
+    print i
+
+for v in visited_links:
+    print v

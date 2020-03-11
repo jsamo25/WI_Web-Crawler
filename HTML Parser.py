a21db662e04320158228957943684cd405848e3b
Jorge <jsamo25@gmail.com>
2020-03-11 17:16:21 +0100

    New structure: crawling based on one url, max counter and respecting /robots.txt

--- HTML Parser.py
+++ HTML Parser.py
@@ -1,76 +1,46 @@
-# HTML Parser: https://docs.python.org/3/library/html.parser.html
-# Web Scrapper: http://scrapingauthority.com/2016/08/25/web-scraping-beautifulsoup/
-# Check on www.website/robots.txt
-
+import urllib.request
+import re
+from urllib import robotparser
 from collections import deque
 from bs4 import BeautifulSoup
-from urlparse import urljoin
-import sys
-import urllib2
-from protego import Protego
-import requests
-from pdb import set_trace
+
+rp = robotparser.RobotFileParser()
+
+def check_robot_txt(url):
+    rp.set_url((url + "/robots.txt"))
+    rp.read()
+    return rp.can_fetch("*",url)
+
+def crawler(url):
+    link_list = []
+    url_file = urllib.request.urlopen(url)
+    url_content = url_file.read()
+    url_file.close()
+
+    soup = BeautifulSoup(url_content,features="lxml")
+    for links in soup.find_all("a", href = re.compile("http://")):
+        link_list.append(links.get("href"))
+    return link_list
 
 
-#url = raw_input("Input the url to start the crawling (example: http://www.google.com ):")
-url = "http://www.google.com"
-queue, visited_links= deque([]), []
+def BrFS_Search(url, max_count):
+    visited, queue, count = set(), deque(), 1
+    queue.append(url) #begining of the search
+    while queue and count < max_count + 1:
+        url = queue.popleft() #extract one element from the left
 
+        if url in visited:
+            continue
+        if check_robot_txt(url) is not True:
+            continue
 
-def bfs_crawler(url):
-    count_limit = 20
-    visited_links.append(url)
-    if len(queue) > count_limit:
-        return
+        visited.add(url)
+        queue.extend(crawler(url))
+        print("[%s] %s"%(count,url))
+        count += 1
 
-    url_file = urllib2.urlopen(url)
-    soup = BeautifulSoup(url_file.read())
-    url_list = soup.findAll("a", href=True)
-
-    for link in url_list:
-        flag = 0
-        # Complete relative URLs and strip trailing slash
-        complete_url = urljoin(url, link["href"]).rstrip('/')
-
-        if robot_txt_allowed(link["href"]) is True:
-            print ("this link is allowed", link)
-
-            for j in queue: # Check if the URL already exists in the queue
-                if j == complete_url:
-                    flag = 1
-                    break
-            # If not found in queue
-            if flag == 0:
-                if len(queue) > count_limit:
-                    return
-                if (visited_links.count(complete_url)) == 0:
-                    queue.append(complete_url)
-
-        else:
-            skip = queue.popleft()
-            print("this link was not allowed",skip)
-
-    current = queue.popleft()
-    bfs_crawler(current)
-
-
-def robot_txt_allowed(link):
-    try:
-        r = requests.get(str(link)+"/robots.txt")
-        rp = Protego.parse(r.text)
-        return rp.can_fetch(link,"*")
-    except:
-        return False
-
-def main():
-
-    bfs_crawler(url)
-    # Print queue
-    print("\n\nLinks crawled:\n")
-    for i in queue:
-        print i
-    for v in visited_links:
-        print v
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    url = "http://" + input("input a URL (example: www.upf.com) ---> ")
+    max_count= int(input("how many links would you like to include on the search?: "))
+    check_robot_txt(url)
+    BrFS_Search(url,max_count)
